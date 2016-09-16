687884d6b076a7ecaf8fff2d666eeb72f2890784
Wagner <wwwgasparsinho@gmail.com>
2016-09-15 23:15:20 -0300

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -35,7 +35,7 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('blog.views.post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
